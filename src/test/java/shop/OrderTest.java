5d00561fb611b4645949b61877a7d170850212d4
Marcelo Gutierrez <marceloaldo@gmail.com>
2014-04-23 18:50:18 -0300

    Implementando Stub Doble

--- src/test/java/shop/OrderTest.java
+++ src/test/java/shop/OrderTest.java
@@ -22,4 +22,49 @@ public class OrderTest {
 		
 		assertNotNull(o_O);
 	}
+
+	
+	@Test
+	public void test03()
+	{
+		IShopDataAccess data = new DummyShopDataAccess();
+		Order o_O = new Order(1, data);
+		
+		o_O.getOrderLines().addOrderLine(1, 3);
+		o_O.getOrderLines().addOrderLine(2, 1);
+		
+		assertEquals(2, o_O.getOrderLines().size());
+	}
+
+	@Test
+	public void testTotal()
+	{
+		IShopDataAccess data = new StubShopDataAccess();
+		Order o_O = new Order(1, data);
+		
+		o_O.getOrderLines().addOrderLine(1, 1);
+		
+		double total = o_O.getOrderLines().get(0).getTotal();
+		
+		assertEquals(25, total, 0);
+	}
+
+	@Test
+	public void testTotal2()
+	{
+		IShopDataAccess data = new StubShopDataAccess();
+		Order o_O = new Order(5, data);
+		
+		o_O.getOrderLines().addOrderLine(1, 3);
+		o_O.getOrderLines().addOrderLine(2, 5);
+		o_O.getOrderLines().addOrderLine(3, 1);
+		
+		double total1 = o_O.getOrderLines().get(0).getTotal();
+		double total2 = o_O.getOrderLines().get(1).getTotal();
+		double total3 = o_O.getOrderLines().get(2).getTotal();
+		
+		assertEquals(75, total1, 0);
+		assertEquals(125, total2, 0);
+		assertEquals(25, total3, 0);
+	}
 }
---
+++ src/test/java/shop/StubShopDataAccess.java
@@ -0,0 +1,15 @@
+package shop;
+
+public class StubShopDataAccess implements IShopDataAccess {
+
+	public double getProductPrice(int id) {
+		// TODO Auto-generated method stub
+		return 25;
+	}
+
+	public void save(int id, Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
